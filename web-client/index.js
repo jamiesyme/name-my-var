4fb3a80174d120ce306a183dd195b0d0f5ba370e
Jamie Syme <jamie.d.syme@gmail.com>
2017-10-29 15:32:36 -0700

    Framed out new design using placeholders.

--- web-client/index.js
+++ web-client/index.js
@@ -1,17 +1,26 @@
 (function() {
 	var $searchInput = $('.search-input');
-	var $bannerContainer = $('.banner-container');
+	var $main = $('main');
 
 	// When the user starts typing, enable search mode
 	$searchInput.on('keyup', function() {
-		$bannerContainer.addClass('search-mode');
+		if ($searchInput.val().length) {
+			$main.addClass('search-mode');
+		}
 	});
 
 	// When the user stops typing, and there is no search term, disable search
 	// mode
 	$searchInput.on('focusout', function() {
 		if (!$searchInput.val().length) {
-			$bannerContainer.removeClass('search-mode');
+			$main.removeClass('search-mode');
+		}
+	});
+
+	// Allow the user to press enter to unfocus the search input
+	$searchInput.on('keydown', function(e) {
+		if (e.which === 13) {
+			this.blur();
 		}
 	});
 })();
