4dd842d21f0c03f71d08e9535e527320896b3c81
Jamie Syme <jamie.d.syme@gmail.com>
2017-11-02 23:23:53 -0700

    Updated api server response format and implemented json "database" for testing.

--- api-server/server.js
+++ api-server/server.js
@@ -2,6 +2,9 @@ var Hapi = require('hapi');
 var Joi = require('joi');
 var Boom = require('boom');
 
+var dbFilename = process.env.DB_FILENAME || './test-data.json';
+var dbJson = require(dbFilename);
+
 var server = new Hapi.Server();
 server.connection({
 	port: 8001
@@ -11,20 +14,19 @@ server.route({
 	method: 'GET',
 	path: '/search',
 	handler: function(request, reply) {
-		var originalWord = request.query.q;
-		var lowerWord = originalWord.toLowerCase();
+		var originalTerm = request.query.q;
+		var normalizedTerm = normalizeTerm(originalTerm);
 
-		if (lowerWord === 'command') {
-			return reply({
-				synonyms: [ 'Command', 'Directive', 'Operation', 'Order' ],
-				relatedWords: [ 'Commander', 'Mission', 'Processor' ],
-				abbreviations: [ 'Cmd' ],
-				examples: [ ],
-				definitions: [ ],
-			});
-		}
+		var result = {
+			searchTerm: normalizedTerm,
+			resultTerms: []
+		};
 
-		return reply(Boom.notFound());
+		result.resultTerms = dbJson.terms.filter(function(term) {
+			return term.name === normalizedTerm;
+		});
+
+		return reply(result);
 	},
 	config: {
 		validate: {
@@ -41,3 +43,32 @@ server.start(function(err) {
 	}
 	console.log('Server running at:', server.info.uri);
 });
+
+// Converts terms of different naming standards to a standard, space-separated,
+// lowercase, letter-only naming standard.
+//
+// Examples conversions:
+//   someVar   -> some var
+//   SomeVar   -> some var
+//   some_var  -> some var
+//   some-var  -> some var
+//   some-var4 -> some var
+//   some$var  -> some var
+function normalizeTerm(term) {
+	var normTerm = String(term);
+
+	// Convert kebab-case
+	normTerm = normTerm.replace(/-/g, ' ');
+
+	// Convert underscore_case
+	normTerm = normTerm.replace(/_/g, ' ');
+
+	// Strip non-letters
+	normTerm = normTerm.replace(/[^a-zA-Z]/g, ' ');
+
+	// Convert TitleCase
+	normTerm = normTerm.replace(/([a-z])([A-Z])/g, '$1 $2');
+
+	// Convert everything that's left to lowercase, and condense/trim whitespace
+	return normTerm.toLowerCase().replace(/\s+/g, ' ').trim();
+}
